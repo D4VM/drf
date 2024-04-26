ba36061fe25af5495ff442bcf545460ea04a8c63
D4VM <davit.miru@gmail.com>
2024-04-26 11:52:54 +0400

    orders: changed quantity field

--- order/models.py
+++ order/models.py
@@ -6,7 +6,7 @@ from user.models import CustomUser
 class Order(models.Model):
     user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)
     product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    quantity = models.IntegerField()
+    quantity = models.PositiveIntegerField(default=0)
     status = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
 
